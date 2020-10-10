3ffbd99dba2894eddff28d3b0f9c241b2b675389
dchs52020 <2586515115@qq.com>
2020-10-10 11:52:25 +0800

    服务提供者注册到consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author DC
+ * @Date 2020-10-10
+ */
+@SpringBootApplication
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
