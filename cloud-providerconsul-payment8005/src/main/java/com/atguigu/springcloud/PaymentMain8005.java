2346885654dd490c008a2135a29eb6a177a6352e
dchs52020 <2586515115@qq.com>
2020-10-10 13:28:23 +0800

    多服务提供者注册到consul,RestTemplate负载均衡

---
+++ cloud-providerconsul-payment8005/src/main/java/com/atguigu/springcloud/PaymentMain8005.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author DC
+ * @Date 2020-10-10
+ */
+@SpringBootApplication
+public class PaymentMain8005 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8005.class, args);
+    }
+}
