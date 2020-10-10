5a637865ae1c57b6d3a8540bdd65ce03b6a00e9d
dchs52020 <2586515115@qq.com>
2020-10-10 13:20:13 +0800

    服务消费者注册到consul，并通过RestTemplate调用提供者

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author DC
+ * @Date 2020-10-10
+ */
+@SpringBootApplication
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+}
