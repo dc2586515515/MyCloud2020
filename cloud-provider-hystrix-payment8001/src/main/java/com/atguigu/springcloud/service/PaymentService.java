9e0f39443db2ec03f6e60a1f15e393b9c304335a
dchs52020 <2586515115@qq.com>
2020-10-17 08:57:56 +0800

    [服务熔断]

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.atguigu.springcloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,5 +36,40 @@ public class PaymentService {
     public String paymentInfo_TimeOutHandler(Integer id) {
         return "线程池：" + Thread.currentThread().getName() + "    系统繁忙，请稍后再试，id   " + id + "\t" + "哭了哇呜";
     }
+
+
+    /**
+     * 服务熔断
+     * <p>
+     * 涉及到断路器的三个重要参数:快照时间窗、请求总数阀值、错误百分比阀值
+     * 1∶快照时间窗:断路器确定是否打开需要统计一些请求和错误数据，而统计的时间范围就是快照时间窗，默认为最近的10秒。
+     * <p>
+     * 2;请求总数阀值:在快照时间窗内，请求数量必须满足请求总数阀值才有资格熔断。默认为20，意味着在10秒内，如果该hvstrix命令的调用次数不足20次.
+     * 即使所有的请求都超时或其他原因失败，断路器都不会打开。
+     * <p>
+     * 3:错误百分比阀值:当请求总数在快照时间窗内超过了阀值，比如发生了30次调用，如果在这30次调用中，有15次发生了超时异常，也就是超过
+     * 50%的错误百分比，在默认设定50%阀值情况下，这时候就会将断路器打开。
+     *
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  // 开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),  // 请求次数超过了峰值，熔断器将会关闭
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),  // 时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"), // 失败率达到多少后跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("*****id ");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + "\t" + "," + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "id ,()/~~     id: " + id;
+    }
+
 
 }
