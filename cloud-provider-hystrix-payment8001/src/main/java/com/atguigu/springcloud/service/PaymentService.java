de75af022ab6fcff0307263580c513b933b5be22
dchs52020 <2586515115@qq.com>
2020-10-20 13:20:48 +0800

    注释

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -55,7 +55,7 @@ public class PaymentService {
      */
     @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {
             @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  // 开启断路器
-            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),  // 请求次数超过了峰值，熔断器将会关闭
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),  // 时间范围内需要达到的请求次数
             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),  // 时间范围
             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"), // 失败率达到多少后跳闸
     })
