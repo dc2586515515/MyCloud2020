ce50ee8a0c0196cfbb4ceceb3f9aa562ab80628a
dchs52020 <2586515115@qq.com>
2020-10-24 11:11:23 +0800

    GateWay通过predicate断言匹配规则进行路由

---
+++ cloud-gateway-gateway9527/src/test/java/T.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Description
+ * @Author DC
+ * @Date 2020-10-24
+ */
+public class T {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
