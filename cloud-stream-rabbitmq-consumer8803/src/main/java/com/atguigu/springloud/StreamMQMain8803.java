c6c844750e4a8992c5fd93afd0c7a1bdfe7447f2
dchs52020 <2586515115@qq.com>
2020-10-28 15:25:32 +0800

    自定义两个消费者为A组，和B组

--- cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springloud/StreamMQMain8803.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springloud/StreamMQMain8803.java
@@ -1,13 +1,7 @@
 package com.atguigu.springloud;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.messaging.Message;
-import org.springframework.stereotype.Component;
 
 /**
  * @Description
