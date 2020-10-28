8176aea2a3a1dfb028d4d5288cbefbe0fcd67ea1
dchs52020 <2586515115@qq.com>
2020-10-28 14:42:01 +0800

    SpringCloud Stream消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,6 @@
+package com.atguigu.springcloud;/**
+ * @Description
+ * @Author DC
+ * @Date 2020-10-28
+ */public class StreamMQMain8802 {
+}
---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,6 @@
+package com.atguigu.springcloud.controller;/**
+ * @Description
+ * @Author DC
+ * @Date 2020-10-28
+ */public class ReceiveMessageListenerController {
+}
