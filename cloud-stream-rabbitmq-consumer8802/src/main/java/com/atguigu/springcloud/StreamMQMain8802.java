53ff76af08e6a4f67c631a4e7ce3847258c6a7c0
dchs52020 <2586515115@qq.com>
2020-10-28 14:43:08 +0800

    SpringCloud Stream消息驱动之消费者

--- cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -1,6 +1,16 @@
-package com.atguigu.springcloud;/**
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
  * @Description
  * @Author DC
  * @Date 2020-10-28
- */public class StreamMQMain8802 {
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
 }
