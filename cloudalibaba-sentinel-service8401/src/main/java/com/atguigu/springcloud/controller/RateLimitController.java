45acd36bdb1475170e9f975d652a0d35c3e11338
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2020-12-03 11:47:02 +0800

    SentinelResource注解使用

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author DC
+ * @Date 2020-12-03
+ */
+@RestController
+public class RateLimitController {
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource", blockHandler = "handleException")
+    public CommonResult byResource() {
+        return new CommonResult(200, "按资源名称限流测试OK", new Payment(2020L, "serial001"));
+    }
+
+    public CommonResult handleException(BlockException exception) {
+        return new CommonResult(444, exception.getClass().getCanonicalName() + "\t 服务不可用");
+    }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl") // 这里如果不加blockHandler，但是sentinel流控配置的时候用 byUrl，超过流控阈值就会抛出异常，不会走sentinel默认的流控提示
+    public CommonResult byUrl() {
+        return new CommonResult(200, "按url限流测试OK", new Payment(2020L, "serial002"));
+    }
+
+}
